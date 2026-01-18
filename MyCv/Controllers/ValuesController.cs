17c96ef74526b95dd2b6c13700ae027b4cac9c31
serhatgenc74@gmail.com <serhatgenc74@gmail.com>
2026-01-18 16:53:21 +0300

    Init commit

---
+++ MyCv/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MyCv.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        Claim claim = new Claim(
+            {
+            ClaimTypes.Name, "JohnDoe"
+            ,});
+    }
+}
