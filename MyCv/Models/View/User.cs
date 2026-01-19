f975f27b695c0c026f10cbe24f155d5dd72c3e34
ismailomeroral <ismailoral0808@gmail.com>
2026-01-19 23:37:48 +0300

    ilk gerçek ekleme

---
+++ MyCv/Models/View/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCv.Models.View
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
